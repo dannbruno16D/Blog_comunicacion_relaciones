9f7e1997cae3e681c1e4ea6c6a1512cb935991a4
dannbruno16D <dannakim.park.bpj@gmail.com>
2026-09-19 00:47:56 -0600

    rh

--- Blog_rhumanas/astro.config.mjs
+++ Blog_rhumanas/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tu-sitio.netlify.app', // <--- CAMBIA ESTO por tu link real de Netlify
+	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
